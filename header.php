9546610fec9d9dd273e8780e5439152e9ff73f30
diogn <rino.dwicahyo@kompas.com>
2019-03-21 16:32:58 +0700

    fixing

--- header.php
+++ header.php
@@ -15,44 +15,18 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-
+	<link href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/single/main.css" rel="stylesheet">
+	
 	<?php wp_head(); ?>
 </head>
-
+<!-- ======================
+			HEADER SECTION
+			======================= -->
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'trial' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$trial_description = get_bloginfo( 'description', 'display' );
-			if ( $trial_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $trial_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+  <!-- NAVBAR  -->
+  <div class="flex mb-4">
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'trial' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+  </div>
 
-	<div id="content" class="site-content">
+</body>
